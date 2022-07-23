7e525ddc9855dc68b6b8f2b80b19c54b6d8d2855
Dathan Stone <developer.stone@outlook.com>
2022-07-23 01:44:58 -0700

    Completed the layout and design of the 'about' section. Finished the footer design.

---
+++ javascript/index.js
@@ -0,0 +1,37 @@
+const languageSelectors = document.getElementsByClassName("language-button")
+const codeBlock = document.getElementById("code-block")
+
+let javaSnippet = document.getElementById("java-snippet")
+const javascriptSnippet = document.getElementById("javascript-snippet")
+const kotlinSnippet = document.getElementById("kotlin-snippet")
+
+javascriptSnippet.style.display = "none"
+kotlinSnippet.style.display = "none"
+
+function selectLanguage(language) {
+
+    const code = document.getElementById("code")
+
+        switch(language) {
+            case "java":
+                javaSnippet.style.display = "block"
+                javascriptSnippet.style.display = "none"
+                kotlinSnippet.style.display = "none"
+                break;
+            case "kotlin":
+                kotlinSnippet.style.display = "block"
+                javaSnippet.style.display = "none"
+                javascriptSnippet.style.display = "none"
+                break;
+            case "javascript":
+                javascriptSnippet.style.display = "block"
+                kotlinSnippet.style.display = "none"
+                javaSnippet.style.display = "none"
+                break;
+            case "sql":
+                code.textContent = "Hello             SQL"  
+            break;
+    
+        }
+    
+}
