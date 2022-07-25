45110731e68d314921d0bf3fbfb1efe8ca18b7b1
Dathan Stone <developer.stone@outlook.com>
2022-07-24 17:33:01 -0700

    Added the javacript for toggling the hamburger button for mobile responsiveness. Finished all responsive design.

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,28 @@
+
+//Hamburger Navigation variables
+const hamburgerButton = document.getElementById("hamburger")
+const navList = document.getElementById("nav-links")
+
+//Toggle function for the hamburger button
+function toggleNav() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleNav)
+
+
+//Code Block Variables
 const languageSelectors = document.getElementsByClassName("language-button")
 const codeBlock = document.getElementById("code-block")
-
 const javaSnippet = document.getElementById("java-snippet")
 const javascriptSnippet = document.getElementById("javascript-snippet")
 const kotlinSnippet = document.getElementById("kotlin-snippet")
 
+//initializing the code block display with "Java".
 javascriptSnippet.style.display = "none"
 kotlinSnippet.style.display = "none"
 
+//Based on user clicking a language, this function displays certain code.
 function selectLanguage(language) {
 
     const code = document.getElementById("code")
